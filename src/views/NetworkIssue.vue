6bb0ce68d20a8f39f33ae844ca103b994fb142f3
dzony6969 <dzony6969@users.noreply.github.com>
2021-06-05 14:59:17 +0200

    router setup - load resource before route enter

---
+++ src/views/NetworkIssue.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Look like you have some network connection problem</h1>
+    <router-link :to="{ name: 'event-list' }">Go back home</router-link>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Page not found 404</h1>
+    <p>the {{ resource }} is not here</p>
+    <router-link :to="{ name: 'event-list' }">Back to home page</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    resource: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="css" scoped>
+</style>
